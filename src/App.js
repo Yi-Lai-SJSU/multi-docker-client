daf601ff9cc192422373a8c452ff92e16325c512
yugguo <yugguo@adobe.com>
2020-03-14 13:45:37 -0700

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Fib from './Fib';
+import OtherPage from './OtherPage';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title"> Welcom to React </h1>
+            <Link to="/"> Home </Link>
+            <Link to="/Otherpage">Other Page</Link>
+          </header>
+          <div>
+            <Route exact path='/' component={Fib} />
+            <Route path='/otherpage' component={OtherPage} />
+          </div>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
